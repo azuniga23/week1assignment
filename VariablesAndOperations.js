f5c6f147deab6ffc513ae129e94947532d44b2d3
Francisco Zuniga <alexanderdagreat06@gmail.com>
2023-02-04 20:20:10 -0600

    first commit

---
+++ VariablesAndOperations.js
@@ -0,0 +1,50 @@
+//create a variable to hold the quantity of available plane seats left on a flight
+let availablePlanSeats = 6;
+		
+//create a variable to hold the cost of groceries at checkout
+let costOfGroceries = 23.46;
+
+//create a variable to hold a person's middle initial
+let middleInitial = "E";
+
+//create a variable to hold true if it's hot outside and false if it's cold outside
+let itsHotOutside = false;
+
+//create a variable to hold a customer's first name
+let customerName = "Natalie";
+
+//create a variable to hold a street address
+let streetAddress = "Samantha Street";
+//print all variables to the console
+console.log(availablePlanSeats);
+console.log(costOfGroceries);
+console.log(middleInitial);
+console.log(itsHotOutside);
+console.log(customerName);
+console.log(streetAddress);
+
+
+
+
+//a customer booked 2 plane seats, remove 2 seats from the available seats variable
+availablePlanSeats = availablePlanSeats - 2;
+console.log(availablePlanSeats);
+
+
+//impulse candy bar purchase, add 2.15 to the grocery total
+//costOfGroceries = costOfGroceries + 2.15;
+costOfGroceries = costOfGroceries + 2.15;
+console.log(costOfGroceries);
+
+//birth certificate was printed incorrectly, change the middle initial to something else
+middleInitial ="A";
+console.log(middleInitial);
+
+//the season has changed, update the hot outside variable to be opposite of what it was
+itsHotOutside = true; 
+console.log(itsHotOutside);
+//create a new variable called full name using the customer's first name, the middle initial, and a last name of your choice
+let fullName = "Natalie Victoria Zuniga";
+console.log(fullName);
+
+//print a line to the console that introduces the customer and says they live at the address variable
